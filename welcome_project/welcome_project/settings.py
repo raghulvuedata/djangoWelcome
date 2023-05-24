fcfa41009eca84cdb2f648e65f36b6658bbd1338
raghulvuedata <rahulvuedata@gmail.com>
2023-05-24 11:15:10 +0530

    second day commits

--- welcome_project/welcome_project/settings.py
+++ welcome_project/welcome_project/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+nsc_zx5j26*xks&^6#!=^khnjyktbkgpn9ogx9%=skc_c9noq'
+# SECRET_KEY = 'django-insecure-+nsc_zx5j26*xks&^6#!=^khnjyktbkgpn9ogx9%=skc_c9noq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
